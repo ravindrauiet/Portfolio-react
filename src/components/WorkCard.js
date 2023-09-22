7555127804faba72daddcdf78fb812cc0ed3e7a8
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2023-09-23 00:56:28 +0530

    add new project and read more functionality

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 import "./WorkCardStyles.css";
 
 import { NavLink } from 'react-router-dom';
+  
 
 const WorkCard = (props) => {
+  const [isFullTextVisible, setFullTextVisible] = useState(false);
+
+  const toggleFullText = () => {
+    setFullTextVisible(!isFullTextVisible);
+  };
   return (
     <div className="project-card">
         <img src={props.imgsrc} alt="Project-img" />
         <h2 className="project-title">{props.title}</h2>
         <div className="project-details">
-            <p>{props.text}</p>
+        {isFullTextVisible ? (
+          <p>{props.text}</p>
+        ) : (
+          <p>
+            {props.text.slice(0, 100)} {/* Display the first 100 characters */}
+            {props.text.length > 100 && !isFullTextVisible && (
+              <span>
+                ...
+                <button onClick={toggleFullText} className="read-more-btn">
+                  Read More
+                </button>
+              </span>
+            )}
+          </p>
+        )}
             <div className="pro-btns">
                 <NavLink to={props.view} className="btn">View</NavLink>
                 <NavLink to={props.source} className="btn">Source</NavLink>
@@ -20,4 +40,4 @@ const WorkCard = (props) => {
   )
 }
 
-export default WorkCard
+export default WorkCard;
--- src/components/WorkCradData.js
+++ src/components/WorkCradData.js
@@ -2,35 +2,62 @@ import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
 import pro4 from "../assets/project4.jpg";
+import pro5 from "../assets/project5.png";
+import pro6 from "../assets/project6.png";
+import pro7 from "../assets/project7.png";
 
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "Calculator",
-        text : "Upon visiting the website, users are presented with a digital calculator interface that closely resembles a physical calculator. The calculator consists of a keypad with buttons representing numbers, mathematical operators (such as addition, subtraction, multiplication, and division), and other essential functions.",
+        imgsrc: pro6,
+        title: "RealEstateX - Your Ultimate Property Marketplace",
+        text : "RealEstateX is a cutting-edge web application built to revolutionize the way users buy, sell, and rent properties. This innovative platform leverages the power of React and Tailwind CSS for its frontend, while Firebase serves as the robust backend to ensure a seamless user experience. RealEstateX caters to users looking for their dream homes or lucrative investment opportunities, offering a secure and efficient platform to meet their real estate needs.",
+        view: "https://realtor-clone-react-lyart.vercel.app/",
+        source: "https://github.com/ravindrauiet/realtor-clone-react.git",
+        
+    },
+    {
+        imgsrc: pro5,
+        title: "ShopifyPro - Your Premier E-Commerce Destination",
+        text : "ShopifyPro is an exceptional e-commerce website built on the powerful MERN (MongoDB, Express, React, Node.js) stack, designed to provide an immersive shopping experience for users. With a sleek and intuitive user interface developed using React and Tailwind CSS, and a robust backend powered by MongoDB and Express, ShopifyPro offers a seamless platform for users to discover, explore, and purchase a wide range of products with ease.",
+        view: "https://totalitycorp-frontend-challenge-rqpt.vercel.app/",
+        source: "https://github.com/ravindrauiet/totalitycorp-frontend-challenge.git",
+        
+    },
+    {
+        imgsrc: pro2,
+        title: "NewsPortalX - Your Window to the World's News",
+        text : "NewsPortalX is a dynamic news website built using React, designed to provide users with an informative and engaging platform for staying updated on the latest news and events from around the world. This news portal fetches news articles from the NewsAPI, offering a wide array of categories, including sports, health, business, technology, science, and general news. NewsPortalX is an open-source project, and contributions from the community are highly encouraged to improve and expand its capabilities.",
+        view: "https://github.com/ravindrauiet/news_website_react.git",
+        source: "https://github.com/ravindrauiet/news_website_react.git",
+    },
+    {
+        imgsrc: pro7,
+        title: "AuthX - Your Trusted Authentication Web Application",
+        text : "AuthX is a robust authentication web application built using the MERN (MongoDB, Express, React, Node.js) stack, designed to provide seamless user authentication and profile management. With its user-centric design crafted using React and Tailwind CSS, AuthX delivers a secure and efficient platform for user login and profile management, ensuring that user data remains protected and accessible only to authorized individuals.",
         view: "https://ravindrauiet.github.io/calculator-react/",
         source: "https://github.com/ravindrauiet/calculator-react.git",
         
     },
     {
-        imgsrc: pro2,
-        title: "NewsWebsite",
-        text : "The News Website project is a React-based news portal that fetches and displays news articles from the NewsAPI. It offers a seamless and customizable browsing experience, allowing users to stay informed about a wide range of topics, including sports, health, business, technology, science, and general news. Contributions to the project are welcome.",
-        view: "https://github.com/ravindrauiet/news_website_react.git",
-        source: "https://github.com/ravindrauiet/news_website_react.git",
+        imgsrc: pro1,
+        title: "CalcXpress - Your Digital Calculator Companion",
+        text : "CalcXpress is a user-friendly digital calculator application that brings the familiarity and functionality of a physical calculator to your digital device. Upon visiting the website, users are greeted with an intuitive digital calculator interface, designed to closely resemble the look and feel of a physical calculator. CalcXpress offers a range of mathematical operations and functions, making it a versatile tool for anyone in need of quick and reliable calculations.",
+        view: "https://ravindrauiet.github.io/calculator-react/",
+        source: "https://github.com/ravindrauiet/calculator-react.git",
+        
     },
     {
         imgsrc: pro3,
-        title: "NotesApp",
-        text : "The main functionality of the application revolves around creating, editing, and organizing notes. Users can easily add new notes, specifying titles and content for each note. They can also modify existing notes by updating content as needed.",
+        title: "NoteMaster - Your Personal Note-Taking Assistant",
+        text : "NoteMaster is a versatile note-taking application designed to simplify the process of creating, editing, and organizing notes. Whether you're a student, professional, or anyone in need of an efficient way to capture and manage information, NoteMaster has you covered. This user-friendly application empowers you to create, update, and organize your notes with ease.",
         view: "https://ravindrauiet.github.io/Notes-App-react/",
         source: "https://github.com/ravindrauiet/Notes-App-react.git",
     },
     {
         imgsrc: pro4,
-        title: "Portfolio",
-        text : "inxinsi dcnsii isi sndin inidnciuns sndcsincis iscdnuicnisndic dsiniusnicn cnisniducns isnincini cisnidcni indci uni incinc icnini",
+        title: "PortfolioX-Your Showcase of Creativity and Achievements",
+        text : "PortfoliX is a beautifully crafted portfolio web application designed to showcase your talents, projects, and accomplishments in a stunning and user-friendly way. This dynamic website is built using React and styled with the elegance of Tailwind CSS, offering a seamless and visually captivating experience for both you and your visitors. PortfoliX comprises four main sections: Home, Projects, About, and Contact, each thoughtfully curated to present your work and personality in the best possible light.",
         view: "http://google.com",
         source: "http://google.com",
     },
